e6cdf94af8d20a2ca1c88a13f8135276b4b7d83e
ayamir <lgt986452565@gmail.com>
2021-05-07 14:21:34 +0800

    finish functional_test

--- functional_tests.py
+++ functional_tests.py
@@ -28,14 +28,9 @@ class NewVisitorTest(unittest.TestCase):  # (1)
 
         # she is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
