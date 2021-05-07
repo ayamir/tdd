7396e434598dddea19bbcfd3d9ebb5e60388a66d
ayamir <lgt986452565@gmail.com>
2021-05-07 14:18:59 +0800

    Redirect after POST, and show all items in templates

--- tddapp/views.py
+++ tddapp/views.py
@@ -1,11 +1,25 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from tddapp.models import Item
 
 # Create your views here.
 
 
-def home_page(request : HttpRequest):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items':items})
+
+
+def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
+
+    self.assertIn('A new list item', response.content.decode())
+    self.assertTemplateUsed(response, 'home.html')
