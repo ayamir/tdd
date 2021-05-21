8fd7e316893a7246209f6b1d83e5e00b88b73afe
ayamir <lgt986452565@gmail.com>
2021-05-21 18:42:32 +0800

    Add a URL for creating a new list via POST

--- tddapp/views.py
+++ tddapp/views.py
@@ -5,9 +5,6 @@ from tddapp.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -25,3 +22,8 @@ def test_can_save_a_POST_request(self):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
