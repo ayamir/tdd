1748b48a994556a42ce6fa0f9250bdfceb8b23a2
ayamir <lgt986452565@gmail.com>
2021-05-21 18:26:16 +0800

    new URL's view and template to display lists

--- tddapp/tests.py
+++ tddapp/tests.py
@@ -47,6 +47,10 @@ class ItemModelTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
     def test_display_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
--- tddapp/views.py
+++ tddapp/views.py
@@ -8,9 +8,7 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 
 def test_can_save_a_POST_request(self):
@@ -26,4 +24,4 @@ def test_can_save_a_POST_request(self):
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
