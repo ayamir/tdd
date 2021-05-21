5e089fed63922f3d49af360d79852b35b87eda40
ayamir <lgt986452565@gmail.com>
2021-05-21 15:03:42 +0800

    use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from msedge.selenium_tools import EdgeOptions, Edge
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):  # (1)
+class NewVisitorTest(LiveServerTestCase):  # (1)
     def setUp(self):  # (2)
         options = EdgeOptions()
         options.use_chromium = True
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):  # (1)
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')  # (3)
+        self.browser.get(self.live_server_url)  # (3)
 
         # she is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -41,30 +42,3 @@ class NewVisitorTest(unittest.TestCase):  # (1)
             '2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')  # (5)
-
-
-if __name__ == '__main__':  # (6)
-    unittest.main(warnings='ignore')  # (7)
-
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage.
-
-# She is invited to enter a to-do item straight away
-
-# She types "Buy peacock features" into a text box (Edith's hobby
-# is tying fly-fishing lures)
-
-# when she hits enter, the page updates, and now the page list
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
-# The page updates again, and now shows both item on her list
-
-# Edith wonder whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
-
-# She visits that URL - her to-do list is still there.
-# Satisfied, she goes back to sleep
