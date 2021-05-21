c770bf045fc8ae281d70da79767ad172070092ce
ayamir <lgt986452565@gmail.com>
2021-05-21 18:58:08 +0800

    Adjust model so that items are associated with different lists

--- tddapp/views.py
+++ tddapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from tddapp.models import Item
+from tddapp.models import Item, List
 
 # Create your views here.
 
@@ -25,5 +25,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
