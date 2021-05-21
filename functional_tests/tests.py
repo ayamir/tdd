cd14be44dd6ec0a67bb84c987a20108442c22278
ayamir <lgt986452565@gmail.com>
2021-05-21 15:34:33 +0800

    add Edith and Francis

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,15 @@
+from socket import SO_MARK
 from django.test import LiveServerTestCase
 from msedge.selenium_tools import EdgeOptions, Edge
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.common.exceptions import WebDriverException
 import time
 
+MAX_TIME = 10
 
-class NewVisitorTest(LiveServerTestCase):  # (1)
-    def setUp(self):  # (2)
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
         options = EdgeOptions()
         options.use_chromium = True
         options.binary_location = r'/usr/bin/microsoft-edge-dev'
@@ -15,30 +18,88 @@ class NewVisitorTest(LiveServerTestCase):  # (1)
         webdriver_path = r'/home/ayamir/.local/bin/msedgedriver'
         self.browser = Edge(options=options, executable_path=webdriver_path)
 
-    def tearDown(self):  # (3)
+    def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if (time.time() - start_time) > MAX_TIME:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)  # (3)
+        self.browser.get(self.live_server_url)
 
-        # she is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')  # (5)
+        self.fail('Finish the test!')
+
+    def test_can_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table(
+            '1: Use peacock feathers to make a fly')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes along to the site.
+
+        # We use a new browser session to make sure that no information
+        # of Edith's is coming through from cookies etc
+        self.browser.quit()
+
+        options = EdgeOptions()
+        options.use_chromium = True
+        options.binary_location = r'/usr/bin/microsoft-edge-dev'
+        options.set_capability("platform", "LINUX")  # (2)
+
+        webdriver_path = r'/home/ayamir/.local/bin/msedgedriver'
+        self.browser = Edge(options=options, executable_path=webdriver_path)
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item.
+        # He is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
